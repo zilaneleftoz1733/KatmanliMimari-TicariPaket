c9c6e13ade59bf22f088e546326944406639e77a
zilanelefoz <lzilaneleftozl@gmail.com>
2024-10-11 14:40:18 +0300

    Added Repository

---
+++ Ticari.DataAccsessLayer/GenericRepository/ECore/Abstract/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Ticari.Entities.Entities.Abstract;
+
+namespace Ticari.DataAccsessLayer.GenericRepository.ECore.Abstract
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        public int Create(T entity);
+        public int Update(T entity);
+        public int Delete(T entity);
+
+        public T GetById(int id);
+        public List<T>? GetAll(Expression<Func<T,bool>>predicate=null);//(sorgu yazabilmek için bu sorguyu vermek lazım p öyledir ki gibi
+
+        public IQueryable<T>? GetAllInclude(Expression<Func<T, bool>> predicate = null, params Expression<Func<T, object>>[] include);
+    }
+}
